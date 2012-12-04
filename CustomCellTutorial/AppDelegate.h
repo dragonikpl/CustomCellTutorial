433de21a70e8f44fe08c051de7d2475b67b7cc6f
Lukasz Domaradzki <dragonikpl@gmail.com>
2012-12-04 20:56:41 +0100

    Initial Commit

---
+++ CustomCellTutorial/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CustomCellTutorial
+//
+//  Created by dragon on 04.12.2012.
+//  Copyright (c) 2012 iOSCook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CustomCellTutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomCellTutorial
+//
+//  Created by dragon on 04.12.2012.
+//  Copyright (c) 2012 iOSCook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
